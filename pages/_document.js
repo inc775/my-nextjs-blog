76ad8c22d6a1e4450681961ae28dc282fd7871ce
inc775 <34394264+inc775@users.noreply.github.com>
2021-05-21 04:33:44 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+      <!-- Cloudflare Web Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "20f1769472ef4251928666e9cc23cef2"}'></script><!-- End Cloudflare Web Analytics -->
         </body>
       </Html>
     );
